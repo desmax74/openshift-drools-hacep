06a85e0ea55b5efdef0e36335801365d62543f12
Max <desmax74@yahoo.it>
2019-03-19 16:25:16 +0100

    polished

--- thorntail/src/main/java/org/kie/u212/endpoint/RestApplication.java
+++ thorntail/src/main/java/org/kie/u212/endpoint/RestApplication.java
@@ -17,12 +17,23 @@ package org.kie.u212.endpoint;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 @ApplicationPath("/rest")
 public class RestApplication extends Application {
 
   @Context
   ServletContext servletContext;
+
+  @GET
+  @Path("/hello")
+  @Produces("text/plain")
+  public Response doGet() {
+    return Response.ok("Hello from Thorntail!").build();
+  }
 }
