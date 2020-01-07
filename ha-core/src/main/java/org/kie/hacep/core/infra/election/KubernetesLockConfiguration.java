f3e99d6690801d9af7a1669d457baf5764a22138
Max <desmax74@yahoo.it>
2020-01-07 16:36:47 +0100

    moved permissions and fix sonarcloud warning

--- ha-core/src/main/java/org/kie/hacep/core/infra/election/KubernetesLockConfiguration.java
+++ ha-core/src/main/java/org/kie/hacep/core/infra/election/KubernetesLockConfiguration.java
@@ -178,7 +178,7 @@ public class KubernetesLockConfiguration implements Cloneable {
 
   @Override
   protected Object clone() throws CloneNotSupportedException {
-    return this.clone();
+    return super.clone();
   }
 
   @Override
--- jdkhttp/scripts/start.sh
+++ jdkhttp/scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "---------Starting Maven Script---------"
-chmod a+x /deployments/maven.sh
 /deployments/maven.sh
 echo "---------End Maven Script---------"
 echo "---------Starting App---------"
