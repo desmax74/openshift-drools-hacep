b1255c90f6a54cf5207915f5d87686ae47475c10
Max <desmax74@yahoo.it>
2020-01-07 13:03:15 +0100

    remove a sonarcloud warning

--- ha-core/src/main/java/org/kie/hacep/consumer/BidirectionalMap.java
+++ ha-core/src/main/java/org/kie/hacep/consumer/BidirectionalMap.java
@@ -20,6 +20,9 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class BidirectionalMap<K, V> extends HashMap<K, V> implements Serializable {
 
   private final Map<V, K> inversedMap = new HashMap();
@@ -47,4 +50,29 @@ public class BidirectionalMap<K, V> extends HashMap<K, V> implements Serializabl
     return key;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (!(o instanceof BidirectionalMap)) {
+      return false;
+    }
+
+    BidirectionalMap<?, ?> map = (BidirectionalMap<?, ?>) o;
+
+    return new EqualsBuilder()
+            .appendSuper(super.equals(o))
+            .append(inversedMap, map.inversedMap)
+            .isEquals();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder(17, 37)
+            .appendSuper(super.hashCode())
+            .append(inversedMap)
+            .toHashCode();
+  }
 }
